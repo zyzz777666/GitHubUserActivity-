89385d113c81dbf2300a246c965958f221521203
I <bumbum.bug@yandex.ru>
2024-10-05 13:59:33 +0300

    init

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import requests
+import pprint
 def main():
-    pass
+    username = 'zyzz777666'
+    url = f'https://api.github.com/users/{username}/events'
+    data = requests.get(url).json()
+
+    for i in data:
+        print(i['type'], i['repo']['name'])
 
 
 if __name__ == '__main__':
